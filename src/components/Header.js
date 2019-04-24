6c72a05bc9f3ae333f90f35909932e50cdec595a
edgaresdras <36061735+edgaresdras@users.noreply.github.com>
2019-04-24 14:07:34 -0500

    Creacion de app y estilos

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+     return ( <
+          header className = "uk-margin" >
+          <
+          h1 className = "uk-text-center" > Eventos < /h1> < /
+          header >
+     );
+}
+
+export default Header;
+
+//Falta uk-margin="true"
