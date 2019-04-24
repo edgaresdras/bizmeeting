401ad51f534e68f3372b606fc9fdbed4df4fcdda
edgaresdras <36061735+edgaresdras@users.noreply.github.com>
2019-04-24 17:28:57 -0500

    guardando las categorias en el state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import Header from "./components/Header";
 
 class App extends Component {
 
+     token = "TSAFR3JKCDBRNTZWJRKE";
+
+     state = {
+          categorias: []
+     }
+
      componentDidMount() {
           this.obtenerCategorias();
      }
 
      obtenerCategorias = async () => {
-          let url= `https://www.eventbriteapi.com/v3/categories/?token=TSAFR3JKCDBRNTZWJRKE&locale=es_ES`;
+          let url= `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
 
           await fetch(url)
                .then(respuesta => {
                     return respuesta.json();
                })
                .then(categorias => {
-                    console.log(categorias.categories);
+                    this.setState({
+                         categorias: categorias.categories
+                    })
                })     
      }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Header = () => {
-     return ( <
-          header className = "uk-margin" >
-          <
-          h1 className = "uk-text-center" > Eventos < /h1> < /
-          header >
+     return ( 
+          <header className="uk-margin" uk-margin="true">
+          <h1 className="uk-text-center"> Eventos </h1> 
+          </header>
      );
 };
 
