2020f79cf19159a34bc25fda26b968c7d217ea4e
edgaresdras <36061735+edgaresdras@users.noreply.github.com>
2019-04-24 22:05:10 -0500

    Configuracion de formulario

--- src/App.js
+++ src/App.js
@@ -30,6 +30,10 @@ class App extends Component {
                })     
      }
 
+     obtenerEventos = async (busqueda) => {
+          console.log(busqueda);
+     }
+
      render() {
           return ( 
                <div className = "App" >
@@ -37,6 +41,7 @@ class App extends Component {
                     <div className="uk-container">
                     <Formulario
                          categorias={this.state.categorias}
+                         obtenerEventos={this.obtenerEventos}
                     />
                     </div>
                </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,32 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Formulario extends Component {
-     render(){
-          return (
-               <form>
-                    <fieldset className="uk-fieldset uk-margin">
-                         <legend className="uk-legend uk-text-center">
-                              Busca tu evento por nombre o categoría
-                         </legend>
 
-                         <div className="uk-column-1-3@m uk-margin">
-                              <div className="uk-margin" uk-margin="true">
-                                   <input className="uk-input" type="text" placeholder="Nombre de evento o Ciudad" />
-                              </div>
-                              <div className="uk-margin" uk-margin="true">
-                                   <select className="uk-select">
+    nombreEventoRef = React.createRef();
+    categoriaRef = React.createRef();
 
-                                   </select>
-                              </div>
-                              <div className="uk-margin" uk-margin="true">
-                                   <button className="uk-button uk-button-danger">Buscar</button>
-                              </div>                                                    
-                         </div>
+  buscarEvento = (e) => {
+    e.preventDefault();
 
-                    </fieldset>     
-               </form>     
-          );
-     }
+    //Crear el objeto
+    const datosBusqueda = {
+      nombre : this.nombreEventoRef.current.value,
+      categoria : this.categoriaRef.current.value
+    }
+    //console.log(datosBusqueda);
+
+    //Pasarlo por props
+    this.props.obtenerEventos(datosBusqueda);
+  }
+  mostrarOpciones = key => {
+    const categoria = this.props.categorias[key];
+    const { id, name_localized } = categoria;
+    if (!id || !name_localized) return null;
+    return (
+      <option key={id} value={id}>
+        {name_localized}
+      </option>
+    );
+  };
+
+  render() {
+    const categorias = Object.keys(this.props.categorias);
+    console.log(categorias);
+    return (
+      <form onSubmit={this.buscarEvento}>
+        <fieldset className="uk-fieldset uk-margin">
+          <legend className="uk-legend uk-text-center">
+            Busca tu evento por nombre o categoría
+          </legend>
+
+          <div className="uk-column-1-3@m uk-margin">
+            <div className="uk-margin" uk-margin="true">
+              <input ref={this.nombreEventoRef}
+                className="uk-input"
+                type="text"
+                placeholder="Nombre de evento o Ciudad"
+              />
+            </div>
+            <div className="uk-margin" uk-margin="true">
+              <select ref={this.categoriaRef} className="uk-select">
+                {categorias.map(this.mostrarOpciones)}
+              </select>
+            </div>
+            <div className="uk-margin" uk-margin="true">
+              <button className="uk-button uk-button-danger">Buscar</button>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    );
+  }
 }
 
 export default Formulario;
